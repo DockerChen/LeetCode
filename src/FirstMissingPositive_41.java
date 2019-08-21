33a3d390b803b8d6c17d6518a2c1f51617afdd32
DockerChen <dockerchen@163.com>
2019-08-21 21:30:27 +0800

    add JianZhiOffer

---
+++ src/FirstMissingPositive_41.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class FirstMissingPositive_41 {
+    public static void main(String[] args) {
+        int nums[] = {7, 8, 9, 11, 12, -1, 1};
+        System.out.println(firstMissingPositive(nums));
+
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+
+        return 0;
+    }
+
+
+}
+
+
+
